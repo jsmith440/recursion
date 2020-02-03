bb23c6fb53d4c5f3ff315224ace813ef1d969811
jsmith440 <jsmith440@cnm.edu>
2020-02-03 11:22:05 -0700

    affirmitive and Negative test cases

--- src/edu/cnm/deepdive/Palindromes.java
+++ src/edu/cnm/deepdive/Palindromes.java
@@ -2,26 +2,18 @@ package edu.cnm.deepdive;
 
 public final class Palindromes {
 
-  private Palindromes() {
-  }
+  private Palindromes() {}
 
   private static boolean isPalindromeClean(String source) {
     return source.length() <= 1
         || (
-        source.charAt(0) == source.charAt(source.length() - 1))
-        && isPalindromes(source.substring(1, source.length() - 1))
-         );
+        source.charAt(0) == source.charAt(source.length() - 1)
+            && isPalindromeClean(source.substring(1, source.length() - 1))
+    );
   }
 
-  private static boolean isPalindromes(String substring) {
+  public static boolean isPalindrome(String source) {
+    return isPalindromeClean(source.replaceAll("[\\W_]+", ""));
   }
-        
-        
-}
 
 }
-
-public static boolean isPalindrome(Stringsource){
-    }
-
-    }
